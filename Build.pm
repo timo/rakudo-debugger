90cad442a81bfbb57352937b22e0ef8f00f4a55a
Timo Paulssen <timonator@perpetuum-immobile.de>
2013-11-03 03:24:50 +0100

    differentiate $nqp based on the backend in use.

--- Build.pm
+++ Build.pm
@@ -1,7 +1,19 @@
 use Panda::Common;
 use Panda::Builder;
 
-my $nqp        = 'nqp';
+my $nqp;
+given $*VM<name> {
+    when "parrot" {
+        $nqp   = 'nqp-p';
+    }
+    when "jvm" {
+        $nqp   = 'nqp-j';
+        die "don't have a launcher generator for the jvm yet";
+    }
+    default {
+        die "don't know how to handle backend $_";
+    }
+}
 my $parrot     = 'parrot';
 my $pbc_to_exe = 'pbc_to_exe';
 my $executable = $*OS eq 'MSWin32' ?? 'perl6-debug.exe' !! 'perl6-debug';
